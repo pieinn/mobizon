f77561ac433861b8ffc6c443440344e671093f1b
laraketai <g.astakhoff@gmail.com>
2019-01-03 20:24:26 +0600

    Create mobizon.php

---
+++ src/config/mobizon.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'alphaname' => null, //Optional, if you don't have registered alphaname, just skip this param and your message will be sent with our free common alphaname.
+    'secret' => env('MOBIZON_SECRET'), // Your secret API key
+];
