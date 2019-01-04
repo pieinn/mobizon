2c28723625c9dfcabe68824ef2348c3685bcde22
laraketai <g.astakhoff@gmail.com>
2019-01-04 14:22:12 +0600

    Update mobizon.php

--- src/config/mobizon.php
+++ src/config/mobizon.php
@@ -1,5 +1,7 @@
 <?php
 return [
-    'alphaname' => null, //Optional, if you don't have registered alphaname, just skip this param and your message will be sent with our free common alphaname.
+    'host' => env('MOBIZON_HOST'), // Your secret API key
     'secret' => env('MOBIZON_SECRET'), // Your secret API key
+    'alphaname' => null, //Optional, if you don't have registered alphaname, just skip this param and your message will be sent with our free common alphaname.
+
 ];
